9f4e65d383484091c22db4542b29dd39f5f4b8f6
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-08-15 18:00:57 +0300

    Fix linter errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book, onDelete }) => {
-  return (
+const Book = ({ book, onDelete }) => (
     <div>
       <h3>{book.title}</h3>
       <p>{book.author}</p>
       <button type="button" onClick={() => onDelete(book.id)}>Delete</button>
     </div>
   );
-};
 
 Book.propTypes = {
   book: PropTypes.shape({
