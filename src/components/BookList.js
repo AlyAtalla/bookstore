7fe4c3dab3f9b961fe269582f4794e5559147842
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-08-21 11:23:16 +0300

    Edit booklist

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,14 @@
-import React, { useState } from 'react';
-import Book from './Book';
-import BookForm from './BookForm';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Book from './Book'; // Assuming you have a Book component
 
 const BookList = () => {
-  const [books, setBooks] = useState([]);
-
-  const handleDelete = (id) => {
-    setBooks(books.filter((book) => book.id !== id));
-  };
-
-  const handleAddBook = (newBook) => {
-    setBooks([...books, newBook]);
-  };
+  const books = useSelector(state => state.book);
 
   return (
     <div>
-      <h2>Book List</h2>
-      <BookForm onAddBook={handleAddBook} />
-      {books.map((book) => (
-        <Book key={book.id} book={book} onDelete={handleDelete} />
+      {books.map(book => (
+        <Book key={book.item_id} book={book} />
       ))}
     </div>
   );
